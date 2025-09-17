37efbaa4fce9ac7f25082a04ad5e5ce151e664ef
Aayush Chauhan <at290608@gmail.com>
2025-09-17 12:49:06 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // make sure db.json is in root
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api', router); // all routes will start with /api
+server.use('/api', router);
 
-server.listen(3000, () => {
-  console.log('✅ JSON Server is running on port 3000');
-});
+module.exports = server;
