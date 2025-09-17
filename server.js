cdfd727058968847fbea02897ffc31fc0f7d1f4d
Aayush Chauhan <at290608@gmail.com>
2025-09-17 12:51:56 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api', router);
+server.use("/api", router);
 
-module.exports = server;
+
+module.exports = (req, res) => {
+  server(req, res);
+};
